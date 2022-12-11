48052f2131f07b5edbfacbee98011fad582eed3f
Vasco Hilario <vascomouraohilario@gmail.com>
2022-12-11 12:45:21 +0000

    Bug Fixes line 1

--- Arduino.ino
+++ Arduino.ino
@@ -1,8 +1,11 @@
 void setup() {
-    pinmode(LED_BUILTIN, OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-    
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
 }
 
