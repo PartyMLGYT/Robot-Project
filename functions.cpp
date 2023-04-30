d29bdc69312fc7279af0140db0fe8018514306cc
ToniChapeiro13 <antoniogomesemail6@gmail.com>
2023-04-30 01:07:28 +0100

    testa

--- functions.cpp
+++ functions.cpp
@@ -10,7 +10,6 @@ void example_fun(args1, args2){
 */
 
 void findDistance(){
-    // to define the pin uncomment the following line
     // int pingPin = 5;
     
     // establish variables for duration of the ping, and the distance result
@@ -52,7 +51,6 @@ void findDistance(){
 
 
     long microsecondsToInches(long microseconds) {
-        // According to Parallax's datasheet for the PING))), there are 73.746
         // microseconds per inch (i.e. sound travels at 1130 feet per second).
         // This gives the distance travelled by the ping, outbound and return,
         // so we divide by 2 to get the distance of the obstacle.
